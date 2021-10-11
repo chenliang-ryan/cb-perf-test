086c6723b41a5b9be9e23ee49905ee0e76cc4dfa
Liang Chen <chenliang.ryan@gmail.com>
2021-10-11 15:10:57 +0800

    Update Create Tables.sql
    Fix some bugs

--- MSSQL Scripts/Create Tables.sql
+++ MSSQL Scripts/Create Tables.sql
@@ -1,14 +1,19 @@
 USE master;
 GO
 
-CREATE DATABASE poc;
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'poc')
+    BEGIN
+        CREATE DATABASE poc;
+    END
 GO
 
-USE [poc]
+USE poc;
 GO
+
+DROP TABLE IF EXISTS [dbo].[Orders];
 
 CREATE TABLE [dbo].[Orders](
-	[OrderID] [int] NOT NULL,
+	[OrderID] [varchar](20) NOT NULL,
 	[Name] [nvarchar](20) NULL,
 	[Gender] [nvarchar](6) NULL,
 	[Age] [tinyint] NULL,
@@ -19,23 +24,25 @@ CREATE TABLE [dbo].[Orders](
 	[Quantity] [int] NULL,
 	[TxTime] [datetime] NULL,
 	[LastUpdate] [datetime] NULL,
- CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED
 (
 	[OrderID] ASC
-)
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
+GO
+
+DROP TABLE IF EXISTS [dbo].[PerfStats];
 GO
 
 CREATE TABLE [dbo].[PerfStats](
 	[StatsID] [bigint] IDENTITY(1,1) NOT NULL,
+	[OrderID] [varchar](20) NOT NULL,
 	[OpType] [nvarchar](20) NULL,
 	[ResponseTime] [float] NULL,
 	[LastUpdate] [datetime2](7) NULL,
- CONSTRAINT [PK_PerfStats] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_PerfStats] PRIMARY KEY CLUSTERED
 (
 	[StatsID] ASC
-)
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-
